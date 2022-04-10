ead171b0283d98c063717527434cce0431eddc9f
aptgetinfo <deepak.2002.02.16@gmail.com>
2022-04-11 04:58:05 +0530

    constants added

--- scripts/deploy.script.js
+++ scripts/deploy.script.js
@@ -1,5 +1,6 @@
 require("dotenv").config(".env");
 const hre = require("hardhat");
+const fs = require("fs");
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contract with account:", deployer.address);
@@ -11,11 +12,17 @@ async function main() {
   const token = await Token.deploy();
   console.log("Token Deployed to:", token.address);
   await token.deployTransaction.wait([(confirms = 6)]);
-  if (process.env.VERIFY === "verify")
+  if (process.env.VERIFY === "verify") {
+    const data = {
+      address: token.address,
+      abi: JSON.parse(token.interface.format("json")),
+    };
+    fs.writeFileSync("constants/Token.json", JSON.stringify(data));
     await hre.run("verify:verify", {
       address: token.address,
       // constructorArguments: ["My NFT",WETH],
     });
+  }
 }
 main()
   .then(() => process.exit(0))
